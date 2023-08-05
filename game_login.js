da50190a9d3779855453cca69dcb4fce121129ec
kestrelv <128956947+kestrelv@users.noreply.github.com>
2023-08-05 19:09:03 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,11 @@
+
+function addUser() {
+  player1_name = document.getElementById("player1_name_input").value;
+  player2_name = document.getElementById("player2_name_input").value;
+
+  localStorage.setItem("player1", player1);
+  localStorage.setItem("player2", player2);
+
+    window.location = "game_page.html";
+}
+
